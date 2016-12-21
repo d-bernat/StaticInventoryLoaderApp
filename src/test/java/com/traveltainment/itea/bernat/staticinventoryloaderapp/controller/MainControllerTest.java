1200c5d45f4778560de9458a3e73ea88f815df02
bernat <bernat@L-HAM-014.traveltainment.int>
2016-12-21 11:53:45 +0100

    add test

---
+++ src/test/java/com/traveltainment/itea/bernat/staticinventoryloaderapp/controller/MainControllerTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.traveltainment.itea.bernat.staticinventoryloaderapp.controller;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bernat
+ */
+public class MainControllerTest
+{
+    
+    public MainControllerTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of hello method, of class MainController.
+     */
+    @Test
+    public void testHello()
+    {
+        MainController instance = new MainController();
+        String expResult = "OK";
+        String result = instance.hello();
+        assertEquals(expResult, result);
+    }
+    
+}
