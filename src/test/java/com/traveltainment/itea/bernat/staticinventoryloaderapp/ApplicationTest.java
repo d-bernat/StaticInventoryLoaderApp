47077cbd93edba23123c002703c2ed329e8328d2
bernat <bernat@L-HAM-014.traveltainment.int>
2016-12-22 14:23:57 +0100

    add test

---
+++ src/test/java/com/traveltainment/itea/bernat/staticinventoryloaderapp/ApplicationTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.traveltainment.itea.bernat.staticinventoryloaderapp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author bernat
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class ApplicationTest
+{
+    //@Autowired
+    //DataSource dataSource;
+    
+    public ApplicationTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    /**
+     * Test of main method, of class Application.
+     */
+    @Test
+    public void testMain()
+    {
+    }
+    
+}
